8bff790cea55866012c6a189f8dd773430a3ed97
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-01-26 02:13:05 +0100

    Update banhammer.sh

--- banhammer.sh
+++ banhammer.sh
@@ -3,7 +3,7 @@
 LOG_1=/var/log/syslog
 LOG_2=/var/log/auth.log
 
-sudo cat ${LOG_1} | grep -e "postfix" -e "auth=0/1" | awk '{print $8}' | awk -F'[' '{print$2}' | sed 's/]$//' | sort | uniq > sus_ip_list_syslog.txt
+sudo cat ${LOG_1} | grep -e "auth=0/1" | awk '{print $8}' | awk -F'[' '{print$2}' | sed 's/]$//' | sort | uniq > sus_ip_list_syslog.txt
 sudo cat ${LOG_2} | grep -e "authentication failure" -e "tty=dovecot" | awk '{print $14}' | awk -F'=' '{print $2}' | uniq > sus_ip_list_auth.log.txt
 
 cat ${LOG_1} ${LOG_2} > sus_ip_combined.txt
