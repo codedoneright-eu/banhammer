769eb47aede3230b3d78c684e081fb6fdb6dd956
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-02-09 22:33:40 +0100

    Update banhammer.sh

--- banhammer.sh
+++ banhammer.sh
@@ -3,15 +3,15 @@
 LOG_1=/var/log/syslog
 LOG_2=/var/log/auth.log
 
-sudo cat ${LOG_1} | grep -e "auth=0/1" | awk '{print $8}' | awk -F'[' '{print$2}' | sed 's/]$//' | sort | uniq > sus_ip_list_syslog.txt
-sudo cat ${LOG_2} | grep -e "authentication failure" -e "tty=dovecot" | awk '{print $14}' | awk -F'=' '{print $2}' | uniq > sus_ip_list_auth.log.txt
+sudo cat ${LOG_1} | grep -e "postfix" -e "auth=0/1" | awk '{print $8}' | awk -F'[' '{print$2}' | sed 's/]$//' | sort | uniq > banhammer_syslog.txt
+sudo cat ${LOG_2} | grep -e "authentication failure" -e "tty=dovecot" | awk '{print $14}' | awk -F'=' '{print $2}' | uniq > banhammer_auth.log.txt
 
-cat ${LOG_1} ${LOG_2} > sus_ip_combined.txt
-cat sus_ip_combined.txt | sort | uniq > sus_ip_all.txt
-cat sus_ip_all.txt | sort | uniq > sus_ip_all.txt
+cat banhammer_syslog.txt banhammer_auth.log.txt | grep . > banhammer_combined.txt
+cat banhammer_combined.txt | sort | uniq >> banhammer_all.txt
+cat banhammer_all.txt | sort | uniq > banhammer_block.txt
 
 
 while IFS= read -r ip_to_block
 do
 	sudo iptables -I INPUT -s ${ip_to_block} -j DROP
-done < sus_ip_all.txt
+done < banhammer_block.txt
